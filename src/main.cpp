7736d3bd8e94b79928c64be9ba5f68765b5da132
Ails <saintails@live.com>
2016-03-16 05:27:45 +0000

    the stdout of pnr.elf is now the bitstream

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ extern void pin_init();
 
 place *place_tool;
 
+
+
 int main(int argc,char* argv[])
 {
 	if (argc==1)
@@ -67,8 +69,33 @@ int main(int argc,char* argv[])
 		cerr<<place_tool->cells[i].type;
 		fputc('\n',stderr);
 	}
-	
-	
+	for (int i=0;i<240;i++)
+	if (place_tool->cells[i].in_v.size())
+	{
+		for (int j=0;j<4;j++)
+			fprintf(stdout,"%c",(char)(place_tool->cells[i].in_v[j]%64));
+		
+	}
+	else
+	{
+		fprintf(stdout,"%d",0);
+	}
+
+	for (int i=0;i<240;i++)
+	if (place_tool->cells[i].in_v.size())
+	{
+		int x=0;
+		x=(int)place_tool->cells[i].sreg.to_ulong() ;
+		x=x<<1;
+		x=x|place_tool->cells[i].type;
+		x=x<<1;
+		fprintf(stdout,"%d",x);
+	}
+	else
+	{
+		fprintf(stdout,"%d",0);
+	}
 	delete place_tool;
+
 	return 0;
 }
